84754c162e00e5d2c19bfb7afb5d2ca643a4c0c4
kwillba <kwillba@wgu.edu>
2025-04-27 12:14:26 -0500

    setup fastapi in main and implemented get and post in local_api

--- local_api.py
+++ local_api.py
@@ -3,12 +3,13 @@ import json
 import requests
 
 # TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+base_url = 'http://127.0.0.1:8000'
+r = requests.get(f"{base_url}/")
 
 # TODO: print the status code
-# print()
+print(f'GET Status Code: {r.status_code}')
 # TODO: print the welcome message
-# print()
+print(f'GET Response: {r.json()}')
 
 
 
@@ -30,9 +31,9 @@ data = {
 }
 
 # TODO: send a POST using the data above
-r = None # Your code here
+r = requests.post(f'{base_url}/data/', json=data)
 
 # TODO: print the status code
-# print()
+print(f'POST Status Code: {r.status_code}')
 # TODO: print the result
-# print()
+print(f'POST Response: {r.json()}')
